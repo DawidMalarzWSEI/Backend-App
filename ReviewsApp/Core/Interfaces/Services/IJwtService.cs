2c211629b5d472a42aee1dd75dbd425cf56aa88e
DawidMalarzWSEI <dawid.malarz@microsoft.wsei.edu.pl>
2023-04-15 09:22:28 +0200

    Add jwt and password services

---
+++ ReviewsApp/Core/Interfaces/Services/IJwtService.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities.Models;
+
+namespace Core.Interfaces.Services;
+
+public interface IJwtService
+{
+    string GenerateAccessToken(User user);
+    RefreshToken GenerateRefreshToken(User user);
+    bool Verify(string token);
+}
---
+++ ReviewsApp/Core/Interfaces/Services/IPasswordService.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Interfaces.Services;
+
+public interface IPasswordService
+{
+    string HashPassword(string password, out byte[] salt);
+    bool VerifyPassword(string password, string hash, byte[] salt);
+}
