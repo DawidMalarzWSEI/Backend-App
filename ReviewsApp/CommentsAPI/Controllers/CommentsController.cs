3660fbe3c260ef91a9f02ba75d03f12ead6be583
DawidMalarzWSEI <dawid.malarz@microsoft.wsei.edu.pl>
2023-05-01 07:42:36 +0200

    Implement comments controller

---
+++ ReviewsApp/CommentsAPI/Controllers/CommentsController.cs
@@ -0,0 +1,92 @@
+﻿using System.Security.Claims;
+using CommentsAPI.Dto.Comment;
+using CommentsAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommentsAPI.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class CommentsController : ControllerBase
+{
+    private readonly ICommentService _commentService;
+
+    public CommentsController(ICommentService commentService)
+    {
+        _commentService = commentService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CommentDto>>> GetAllComments()
+    {
+        return Ok(await _commentService.GetAllAsync());
+    }
+
+    [HttpGet("{commentId:guid}")]
+    public async Task<ActionResult<CommentDto>> GetComment(Guid commentId)
+    {
+        var comment = await _commentService.GetByIdAsync(commentId);
+        if (comment == null) return NotFound();
+        return Ok(comment);
+    }
+
+    [HttpGet("GetMyComments")]
+    [Authorize]
+    public async Task<ActionResult<IEnumerable<CommentDto>>> GetMyComments()
+    {
+        var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+        var comments = await _commentService.GetAllByUserIdAsync(userId);
+        if (comments is null) return NotFound();
+
+        return Ok(comments);
+    }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<ActionResult<CommentDto>> PostComment(CommentCreateDto commentCreateDto)
+    {
+        var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+        var newComment = await _commentService.CreateAsync(userId, commentCreateDto);
+
+        return CreatedAtAction(nameof(GetComment), new {commentId = newComment.Id}, newComment);
+    }
+
+    [HttpPut("{commentId:guid}")]
+    [Authorize]
+    public async Task<IActionResult> PutComment(Guid commentId, CommentUpdateDto commentUpdateDto)
+    {
+        var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        var role = User.FindFirstValue(ClaimTypes.Role);
+
+        var comment = await _commentService.GetByIdAsync(commentId);
+
+        if (comment == null) return NotFound();
+        if (!comment.UserId.Equals(userId) && role != "Administrator")
+            return Unauthorized(new {error_message = "The comment does not belong to this user"});
+
+        await _commentService.UpdateAsync(commentId, commentUpdateDto);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{commentId:guid}")]
+    [Authorize]
+    public async Task<IActionResult> DeleteComment(Guid commentId)
+    {
+        var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        var role = User.FindFirstValue(ClaimTypes.Role);
+
+        var comment = await _commentService.GetByIdAsync(commentId);
+
+        if (comment == null) return NotFound();
+        if (!comment.UserId.Equals(userId) && role != "Administrator")
+            return Unauthorized(new {error_message = "The comment does not belong to this user"});
+
+        await _commentService.DeleteAsync(commentId);
+
+        return NoContent();
+    }
+}
--- ReviewsApp/CommentsAPI/Program.cs
+++ ReviewsApp/CommentsAPI/Program.cs
@@ -1,11 +1,39 @@
+using System.Text.Json.Serialization;
+using CommentsAPI.AsyncDataService;
+using CommentsAPI.EventProcessing;
+using CommentsAPI.Services;
+using Core.Configuration;
+using Core.Entities.Models;
+using Core.Interfaces.Repositories;
+using Core.Interfaces.Services;
+using Infrastructure.Configuration;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Services.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// JWT
+builder.Services.AddSingleton<JwtSettings>();
+builder.Services.ConfigureJwt(new JwtSettings(builder.Configuration));
+builder.Services.AddScoped<IJwtService, JwtService>();
+
+// Cors and swagger
+builder.Services.ConfigureCors();
+builder.Services.ConfigureSwagger();
+
+// Database
+builder.Services.ConfigureMongo(builder.Configuration);
+builder.Services.AddSingleton<IMongoDbContext, MongoDbContext>();
+builder.Services.AddTransient<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<ICommentService, CommentService>();
+builder.Services.AddTransient<IEventProcessor, EventProcessor>();
+
+// DataService
+builder.Services.AddHostedService<MessageBusCommentSubscriber>();
 
 var app = builder.Build();
 
@@ -14,10 +42,16 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    // Seed database
+    SeedDatabase<Comment>.Seed(app);
 }
 
 app.UseHttpsRedirection();
 
+app.UseHealthChecks("/healthcheck");
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
