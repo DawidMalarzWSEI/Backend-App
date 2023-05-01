dc13211a739564e7d565d10a1ee16c35101774af
DawidMalarzWSEI <dawid.malarz@microsoft.wsei.edu.pl>
2023-05-01 11:07:14 +0200

    Add comments DTOs

---
+++ ReviewsApp/CommentsAPI/Dto/Comment/CommentDto.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+
+namespace CommentsAPI.Dto.Comment;
+
+public class CommentDto : IEntityBase
+{
+    public string Text { get; init; }
+    public DateTimeOffset CreatedAt { get; init; }
+    public Guid ReviewId { get; init; }
+    public Guid UserId { get; init; }
+    public Guid Id { get; init; }
+
+    public static CommentDto CommentToDto(Core.Entities.Models.Comment comment)
+    {
+        return new CommentDto
+        {
+            Id = comment.Id,
+            Text = comment.Text,
+            CreatedAt = comment.CreatedAt,
+            ReviewId = comment.ReviewId,
+            UserId = comment.UserId
+        };
+    }
+}
