0e620448fb0e8141e2e892fb1faa32d0c9baa742
DawidMalarzWSEI <Pawel.Majkut@microsoft.wsei.edu.pl>
2023-06-15 19:21:44 +0200

    Implement Integration Tests

---
+++ ReviewsApp/ReviewsIntegrationTests/CustomWebAppFactory.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Claims;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ReviewsIntegrationTests;
+
+public class CustomWebAppFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        var configPath = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+
+        builder.ConfigureAppConfiguration((context, conf) => { conf.AddJsonFile(configPath); });
+
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll(typeof(IHostedService));
+
+            services.AddAuthentication(o =>
+            {
+                o.DefaultAuthenticateScheme = "TestScheme";
+                o.DefaultChallengeScheme = "TestScheme";
+            }).AddScheme<AuthenticationSchemeOptions, TestAuthHandler>("TestScheme", _ => { });
+        });
+        builder.UseEnvironment("Development");
+
+        base.ConfigureWebHost(builder);
+    }
+}
+
+
+public class TestAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+{
+    public TestAuthHandler(IOptionsMonitor<AuthenticationSchemeOptions> options,
+        ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+        : base(options, logger, encoder, clock)
+    {
+    }
+
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.Name, "Test user"),
+            new Claim(ClaimTypes.NameIdentifier, "userId"),
+            new Claim(ClaimTypes.Email, "user@example.com")
+        };
+        var identity = new ClaimsIdentity(claims, "Test");
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal, "TestScheme");
+
+        var result = AuthenticateResult.Success(ticket);
+
+        return Task.FromResult(result);
+    }
+}
---
+++ ReviewsApp/ReviewsIntegrationTests/MongoDbFixture.cs
@@ -0,0 +1,30 @@
+﻿using Core.Entities.Models;
+using MongoDB.Driver;
+
+namespace ReviewsIntegrationTests;
+
+public class MongoDbFixture : IDisposable
+{
+    private readonly IMongoClient _mongoClient;
+    private readonly IMongoDatabase _mongoDatabase;
+    private readonly IMongoCollection<Review> _collection;
+    private const string CollectionName = "Reviews";
+    private const string DatabaseName = "Reviews_integration_tests";
+
+    public MongoDbFixture()
+    {
+        _mongoClient = new MongoClient("mongodb://localhost:27017");
+        _mongoDatabase = _mongoClient.GetDatabase(DatabaseName);
+        _collection = _mongoDatabase.GetCollection<Review>(CollectionName);
+    }
+
+    public Task InsertAsync<TEntity>(string collectionName, TEntity entity)
+    {
+        return _mongoDatabase.GetCollection<TEntity>(collectionName).InsertOneAsync(entity);
+    }
+
+    public void Dispose()
+    {
+        _mongoDatabase.Client.DropDatabase(DatabaseName);
+    }
+}
